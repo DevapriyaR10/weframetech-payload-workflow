c7d7aa383f69a9e74a9b71eab9f810bccc6525ec
Deva Priya R <138274748+DevapriyaR10@users.noreply.github.com>
2026-03-09 12:12:13 +0530

    Delete src/components/BlogLexicalField.tsx

--- src/components/BlogLexicalField.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-
-// Payload v3 custom field type can be React.FC<any>
-const BlogLexicalField: React.FC<any> = (props) => lexicalEditor(props)
-
-export default BlogLexicalField
