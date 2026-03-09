36ed04badbbe171d840a1e5f2cd73f20cfe1e808
Deva Priya R <138274748+DevapriyaR10@users.noreply.github.com>
2026-03-09 11:59:26 +0530

    Update BlogLexicalField.tsx

--- src/components/BlogLexicalField.tsx
+++ src/components/BlogLexicalField.tsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import type { Field } from 'payload/types'
 
+const BlogLexicalField: Field = (props) => {
+  return lexicalEditor(props)
+}
+
+export default BlogLexicalField
