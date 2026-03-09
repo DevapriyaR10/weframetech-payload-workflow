5f3e167cbf0046a3b99117ee64a6f903bd498663
Deva Priya R <138274748+DevapriyaR10@users.noreply.github.com>
2026-03-09 09:02:08 +0530

    Update Users.ts

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -3,9 +3,49 @@ import type { CollectionConfig } from 'payload'
 export const Users: CollectionConfig = {
   slug: 'users',
   auth: true,
-  admin: { useAsTitle: 'email' },
+
+  admin: {
+    useAsTitle: 'email',
+  },
+
+  access: {
+    
+    create: ({ req }) => {
+      return req.user?.role === 'admin'
+    },
+
+   
+    delete: ({ req }) => {
+      return req.user?.role === 'admin'
+    },
+
+    
+    read: ({ req }) => {
+      if (req.user?.role === 'admin') return true
+      return {
+        id: {
+          equals: req.user?.id,
+        },
+      }
+    },
+
+  
+    update: ({ req }) => {
+      if (req.user?.role === 'admin') return true
+      return {
+        id: {
+          equals: req.user?.id,
+        },
+      }
+    },
+  },
+
   fields: [
-    { name: 'name', type: 'text', required: true },
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
     {
       name: 'role',
       type: 'select',
